73aef1cdddde70196027787a9e1bc12e15679242
Robert Kettler <kettler.robert@gmail.com>
2014-06-16 11:11:24 +0200

    added shader Machete.blend and WeaponBehavior.js

---
+++ Assets/NewBehaviourScript2.js
@@ -0,0 +1,21 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+function Update () {
+
+}
+
+function OnTriggerEnter (other : Collider) {
+	Debug.Log('test');
+}
+
+function onCollisionEnter (other : Collision) {
+	Debug.Log('test');
+}
+
+function onControllerColliderHit (other : ControllerColliderHit) {
+	Debug.Log('test');
+}
---
+++ Assets/ObjectInteraction.js
@@ -0,0 +1,28 @@
+﻿#pragma strict
+
+public var forwardDistance : int = 4;
+public var upDistance : int = 2;
+
+var currentObject : GameObject = null;
+
+function Start () {
+
+}
+
+function Update () {
+
+}
+
+function OnControllerColliderHit (hit : ControllerColliderHit) {
+	if(hit.gameObject.tag == 'pickup') {
+		
+		if(currentObject != null){
+			currentObject.gameObject.transform.position = gameObject.transform.position + (gameObject.transform.forward * forwardDistance)+ (gameObject.transform.up * upDistance);
+			currentObject.gameObject.transform.rotation = gameObject.transform.rotation;		
+			currentObject.gameObject.SetActive(true);
+		}
+		
+		hit.gameObject.SetActive(false);
+		currentObject = hit.gameObject;
+	}
+}
